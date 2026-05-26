f73f2e5deb2aa789127a9a4a4ef5408b64cef81e
Rukhsar499 <rukshar.digitalwolf@gmail.com>
2026-05-26 14:11:25 +0530

    hi

--- app/page.tsx
+++ app/page.tsx
@@ -32,7 +32,7 @@ export default function MusicalWorkshopLanding() {
     dietPreference: '',
     accommodation: false,
    pay_description: "Online Booking For Accommodation",
-   total_amount: 2500,
+   total_amount: 1,
    promo_code: "",
    discount_amount: 0
   });
